cb4cb47bd169a696ba8aaa9d1566042c3a41913b
Thomas <thomas@cnt-vm.petrichor.local>
2018-11-30 12:53:46 -0600

    Added functionality to send data via serial on Pi

--- HWFuncs.py
+++ HWFuncs.py
@@ -1,5 +1,13 @@
+
 # contains funcitons needed to call to send messages via the hardware
+import serial
+from base64 import b64encode as base64encode
 
 def send_packet(packet):
     print('Sending the following packet...')
     print(packet)
+    ser = serial.Serial('/dev/ttyAMA0', 1200, timeout=1)
+    encoded_bytes = base64encode(packet.id + ':') + packet.data
+    ser.open()
+    ser.write(str(encoded_bytes))
+    ser.close()
