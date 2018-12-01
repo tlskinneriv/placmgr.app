cb13a08e177374765c2d4424e426355001378c22
Thomas <thomas@cnt-vm.petrichor.local>
2018-11-30 21:47:43 -0600

    Revert last changes

--- HWFuncs.py
+++ HWFuncs.py
@@ -19,12 +19,14 @@ serial_port = '/dev/tty'
 def send_packet(packet):
     encoded_bytes = base64encode((packet.id + ':').encode()) + packet.data
     try:
+        start_IR_clock()
         ser = serial.Serial(serial_port, 1200, timeout=0)
         if ser.isOpen() == False:
             ser.open()
         ser.write(encoded_bytes)
         ser.flush()
         ser.close()
+        stop_IR_clock()
     except Exception as e:
         raise HWFuncException('Could not send via the serial port "' + serial_port + '"', e)
 
