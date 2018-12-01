d8a504a873ced0932edfebe2087721473566496d
Thomas <thomas@cnt-vm.petrichor.local>
2018-11-30 21:34:21 -0600

    update clock usage

--- HWFuncs.py
+++ HWFuncs.py
@@ -32,8 +32,8 @@ def send_packet(packet):
 
 def start_IR_clock():
     pi = pigpio.pi()
-    pi.hardware_PWM(18, 38000, 500000)
+    pi.hardware_clock(4, 38000)
 
 def stop_IR_clock():
     pi = pigpio.pi()
-    pi.hardware_PWM(18, 0, 0)
+    pi.hardware_clock(4, 0)
