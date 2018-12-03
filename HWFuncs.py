83428bd8226d690aa6d55f381b48112bbefa6d24
Thomas <thomas@cnt-vm.petrichor.local>
2018-12-03 13:33:39 -0600

    Removed 38khz clock, not needed for regular transmission.

--- HWFuncs.py
+++ HWFuncs.py
@@ -19,29 +19,13 @@ class HWFuncException(Exception):
 serial_port = '/dev/tty'
 def send_packet(packet):
     encoded_bytes = base64encode((packet.id + ':' + str(len(packet.data)) + ':').encode() + packet.data)
-    print(encoded_bytes)
     tmr_string = b'[[[' + encoded_bytes + b']]][[[' + encoded_bytes + b']]][[[' + encoded_bytes + b']]]'
-    print(tmr_string)
     try:
-        start_IR_clock()
-        time.sleep(.500) #wait for clock to stabilize
         ser = serial.Serial(serial_port, 4800, timeout=0)
         if ser.isOpen() == False:
             ser.open()
         ser.write(tmr_string)
         ser.flush()
         ser.close()
-        time.sleep(.500) # ensure clock is on through finish of serial
-        stop_IR_clock()
     except Exception as e:
         raise HWFuncException('Could not send via the serial port "' + serial_port + '"', e)
-
-def start_IR_clock():
-    pi = pigpio.pi()
-    pi.hardware_clock(4, 38000)
-    pi.stop()
-
-def stop_IR_clock():
-    pi = pigpio.pi()
-    pi.hardware_clock(4, 0)
-    pi.stop()
